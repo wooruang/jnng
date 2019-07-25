5ecec66d2c53b4f9d86aff4fd5cb9ebeb511f6ef
wooruang <wooruang@gmail.com>
2019-07-25 13:20:43 +0900

    Add a initial java project by gradle

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    java
+}
+
+group = "com.wooruang"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testCompile("junit", "junit", "4.12")
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
---
+++ src/main/java/com/wooruang/jnng/JNNG.java
@@ -0,0 +1,9 @@
+package com.wooruang.jnng;
+
+
+public class JNNG {
+    public static void main(String[] args) {
+
+        System.out.println("Hello nng");
+    }
+}
