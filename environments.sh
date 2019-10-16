2909210fe0299ee8c7fe6a22f3cfbe1105516644
wooruang <wooruang@gmail.com>
2019-10-16 20:32:41 +0900

    Modify environments.sh

--- environments.sh
+++ environments.sh
@@ -10,7 +10,59 @@ JDK_URL=$JDK_BASE_URL$JDK_TAR
 JDK_MD5_URL=$JDK_BASE_URL$JDK_MD5
 PATH="${PATH}:/$JDK_VER/bin"
 
-curl -O "$JDK_URL" && curl -O "$JDK_MD5_URL"
+# Exist Check!
+if [[ -f "$JDK_TAR" ]]; then
+  echo "Already exist a $JDK_TAR"
+else
+  echo "Could not find a $JDK_TAR"
+  echo "Download a $JDK_TAR!"
+  curl -O "$JDK_URL"
+fi
 
-echo "`cat $JDK_MD5` $JDK_TAR" > $JDK_MD5 && sha256sum $JDK_TAR | sha256sum --check $JDK_MD5 &&  tar xzf $JDK_TAR && java -version
+if [[ -f "$JDK_MD5" ]]; then
+  echo "Already exist a $JDK_MD5"
+else
+  echo "Could not find a $JDK_MD5"
+  echo "Download a $JDK_MD5!"
+  curl -O "$JDK_MD5_URL"
+fi
+
+# Check OS.
+echo $OSTYPE
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  # Linux
+  echo "Linux"
+#  echo "`cat $JDK_MD5` $JDK_TAR" > $JDK_MD5 && sha256sum $JDK_TAR | sha256sum --check $JDK_MD5
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  # Mac OSX
+  echo "MacOSX"
+  cat $JDK_MD5 | awk -v file=$JDK_TAR '{print $1, "", file }' | shasum -a 256 -c
+#elif [[ "$OSTYPE" == "cygwin" ]]; then
+#        # POSIX compatibility layer and Linux environment emulation for Windows
+#elif [[ "$OSTYPE" == "msys" ]]; then
+#        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+#elif [[ "$OSTYPE" == "win32" ]]; then
+#        # I'm not sure this can happen.
+#elif [[ "$OSTYPE" == "freebsd"* ]]; then
+#        # ...
+else
+  # Unknown.
+  echo "Unknown OS."
+  exit 1
+fi
+
+if [[ $? -ne 0 ]]
+then
+  echo "Fail to check a shasum value"
+  exit 1
+fi
+
+if [[ -d "$JDK_VER" ]]
+then
+  echo "Already extract a tar file. ($JDK_VER)"
+  exit 0
+fi
+
+# Extract a tar file.
+tar xzf $JDK_TAR && java -version
 
